35497f821d3344c3d5bde0d151b96e8b1605ac14
mario.galindo2911 <mario.galindo2911@gmail.com>
2024-03-27 13:43:06 -0600

    first commit

---
+++ src/main/java/com/example/idendityservice/IdendityServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.idendityservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdendityServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdendityServiceApplication.class, args);
+	}
+
+}
